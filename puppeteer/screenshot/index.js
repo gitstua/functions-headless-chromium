6ac9d80edc2cf49d794a8f686b742a6cc1488240
Stuart Eggerton <gitstua@users.noreply.github.com>
2021-04-25 07:24:35 +0000

    updated screenshot to call endpoint 3 times for timing

--- puppeteer/screenshot/index.js
+++ puppeteer/screenshot/index.js
@@ -1,21 +1,37 @@
 const puppeteer = require("puppeteer");
 
 module.exports = async function (context, req) {
-    const url = req.query.url || "https://google.com/";
+    const url = req.query.url || "https://dadstaxi.azurefd.net/";
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    //set a timer then request the page
-    const t1 = Date.now();
+    //time request
+    let t1 = Date.now();
     await page.goto(url);
     const diff1 = Date.now() - t1;
     console.log(`Time: ${diff1}ms`);
-    
+
+    //time request
+    t1 = Date.now();
+    await page.goto(url);
+    const diff2 = Date.now() - t1;
+    console.log(`Time: ${diff2}ms`);
+
+    //time request
+    t1 = Date.now();
+    await page.goto(url);
+    const diff3 = Date.now() - t1;
+    console.log(`Time: ${diff3}ms`);
+
+    //ignore first request as we consider this warm up and take average of 2 subsequent requests as best measure
+    const diffAverage = (diff2 + diff3) /2;
+    console.log(`Average Time: ${diffAverage}ms`);
+
     //write the time taken onto the page so it is show in the screenshot
-    await page.evaluate((diff1) => {
+    await page.evaluate((timetaken) => {
         let dom = document.querySelector('#messages');
-        dom.innerHTML = `Page load time: ${diff1}ms`;
-    });
+        dom.innerHTML = `Page load time: ${timetaken}ms`;
+    }, diffAverage);
 
     //take screenshot
     const screenshotBuffer = await page.screenshot({ fullPage: true });
